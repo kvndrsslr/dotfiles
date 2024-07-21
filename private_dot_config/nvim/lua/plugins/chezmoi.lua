30c7b901ed2e9c47bdb62549e1b8dc12f9b1ba86
Kevin Dreßler <mail@kxfin.xyz>
2024-07-21 11:42:56 +0200

    add nvim chezmoi plugin

--- private_dot_config/nvim/lua/plugins/chezmoi.lua
+++ private_dot_config/nvim/lua/plugins/chezmoi.lua
@@ -2,6 +2,10 @@ return {
   "xvzc/chezmoi.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("chezmoi").setup({})
+    require("chezmoi").setup({
+      edit = {
+        watch = true,
+      },
+    })
   end,
 }
